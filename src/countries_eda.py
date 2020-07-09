a889a9b383f8fd4e644e0cf00fa7a21f13e2a2d6
Sebastian Jaramillo-Riveri <sebajarar@gmail.com>
2020-07-09 19:44:05 -0400

    Organized EDA scripts

--- src/countries_eda.py
+++ src/countries_eda.py
@@ -49,32 +49,28 @@ colors = get_list_colors('rainbow', len(countries))
 countries2id = pd.DataFrame(world_df.groupby('countriesAndTerritories')['geoId'].max())
 countries2abr = pd.DataFrame(world_df.groupby('countriesAndTerritories')['countryterritoryCode'].max())
 
+sizes = 2
 topn = 20
-#maxx = np.max(world_df['dateRep'].unique()-min_date)/np.timedelta64(1, 'D')
-maxx = 150
+maxx = np.max(world_df['dateRep'].unique()-min_date)/np.timedelta64(1, 'D')-38
 
-maxy = 1e07
-miny = 100
-hlcountries = world_df.groupby('countriesAndTerritories')['total_cases'].max().sort_values(ascending=True).index[-topn:].values
-legendys = np.logspace(np.log10(miny), np.log10(maxy), len(hlcountries)+2)
-fig, axes = plt.subplots(ncols=1, nrows=1, figsize=(11, 10))
-for (i, country) in enumerate(countries):
-    sdf = world_df[world_df['countriesAndTerritories'] == country].sort_values('dateRep', ascending=True)
+def get_days(sdf, min_date=min_date):
     days = pd.Series((sdf['dateRep'] - min_date).values/np.timedelta64(1, 'D'))
     if((sdf['total_cases'] > 100).sum() > 0):
         zeroday = (sdf[sdf['total_cases'] > 100]['dateRep'].iloc[0]-min_date)/np.timedelta64(1, 'D')
     else:
-        zeroday = days.max()
-    yval = sdf['total_cases']
+        zeroday = days.max()    
     deltadays = days-zeroday
+    return(days, deltadays)
+
+def country_legend(axes, country, hlcountries, legendys, yval, colors=colors, maxx=maxx):
     if(country in hlcountries):
         color = colors[i]
         abr = countries2abr.loc[country, 'countryterritoryCode']
         ind = np.where(hlcountries == country)[0][0]
         axes.text(1.1*maxx, legendys[ind+1], str(topn-ind)+'. '+abr,
                   horizontalalignment='left', verticalalignment='center', fontsize=14)
-        plt.arrow(deltadays.max(), sdf['total_cases'].iloc[-1],
-                  1.04*maxx-deltadays.max(), legendys[ind+1]-sdf['total_cases'].iloc[-1],
+        plt.arrow(deltadays.max(), yval.iloc[-1],
+                  1.04*maxx-deltadays.max(), legendys[ind+1]-yval.iloc[-1],
                   clip_on=False, shape='right', ls=':', color=color, alpha=0.75)
         geoid = countries2id.loc[country, 'geoId'].lower()
         if(geoid == 'uk'):
@@ -90,7 +86,18 @@ for (i, country) in enumerate(countries):
         axes.add_artist(ab)
     else:
         color = (0.8, 0.8, 0.8, 0.5)
-    sizes = 2
+    return color
+
+maxy = 1e07
+miny = 100
+hlcountries = world_df.groupby('countriesAndTerritories')['total_cases'].max().sort_values(ascending=True).index[-topn:].values
+legendys = np.logspace(np.log10(miny), np.log10(maxy), len(hlcountries)+2)
+fig, axes = plt.subplots(ncols=1, nrows=1, figsize=(11, 10))
+for (i, country) in enumerate(countries):
+    sdf = world_df[world_df['countriesAndTerritories'] == country].sort_values('dateRep', ascending=True)
+    yval = sdf['total_cases']
+    (days, deltadays) = get_days(sdf)
+    color = country_legend(axes, country, hlcountries, legendys, yval, colors=colors, maxx=maxx)
     axes.plot(deltadays, yval, 'o-', c=color, markersize=sizes, markeredgecolor=color)
 local_axes_formatting(axes, "Days since reaching 100 cases", "Total Positive Cases", xlim1=0, xlim2=maxx, ylim1=miny, ylim2=maxy, logx=False, fs=20)
 axes.set_title('Top 20 countries in total positive cases', fontsize=24)
@@ -107,37 +114,9 @@ legendys = np.logspace(np.log10(miny), np.log10(maxy), len(hlcountries)+2)
 fig, axes = plt.subplots(ncols=1, nrows=1, figsize=(11, 10))
 for (i, country) in enumerate(countries):
     sdf = world_df[world_df['countriesAndTerritories'] == country].sort_values('dateRep', ascending=True)
-    days = pd.Series((sdf['dateRep'] - min_date).values/np.timedelta64(1, 'D'))
-    if((sdf['total_cases'] > 100).sum() > 0):
-        zeroday = (sdf[sdf['total_cases'] > 100]['dateRep'].iloc[0]-min_date)/np.timedelta64(1, 'D')
-    else:
-        zeroday = days.max()
     yval = sdf['cases_rolling']
-    deltadays = days-zeroday
-    if(country in hlcountries):
-        color = colors[i]
-        abr = countries2abr.loc[country, 'countryterritoryCode']
-        ind = np.where(hlcountries == country)[0][0]
-        axes.text(1.1*maxx, legendys[ind+1], str(topn-ind)+'. '+abr,
-                  horizontalalignment='left', verticalalignment='center', fontsize=14)
-        plt.arrow(deltadays.max(), yval.iloc[-1],
-                  1.04*maxx-deltadays.max(), legendys[ind+1]-yval.iloc[-1],
-                  clip_on=False, shape='right', ls=':', color=color, alpha=0.75)
-        geoid = countries2id.loc[country, 'geoId'].lower()
-        if(geoid == 'uk'):
-            geoid = 'gb'
-        flagpath = 'circle-flags/flags/'+ geoid +'.png'
-        if(not os.path.isfile(flagpath)):
-            print(flagpath)
-        flagimg = mpimg.imread(flagpath)
-        zoom = 0.05
-        im = OffsetImage(flagimg, zoom=zoom)
-        ab = AnnotationBbox(im, (1.06*maxx, legendys[ind+1]), xycoords='data',
-                            frameon=False, annotation_clip=False)
-        axes.add_artist(ab)
-    else:
-        color = (0.8, 0.8, 0.8, 0.5)
-    sizes = 2
+    (days, deltadays) = get_days(sdf)
+    color = country_legend(axes, country, hlcountries, legendys, yval, colors=colors, maxx=maxx)
     axes.plot(deltadays, yval, 'o-', c=color, markersize=sizes, markeredgecolor=color)
 local_axes_formatting(axes, "Days since reaching 100 cases", "New cases per day (7 days rolling average)", xlim1=0, xlim2=maxx, ylim1=miny, ylim2=maxy, logx=False, fs=20)
 axes.set_title('Top 20 countries in positive cases per day', fontsize=24)
@@ -154,38 +133,9 @@ legendys = np.logspace(np.log10(miny), np.log10(maxy), len(hlcountries)+2)
 fig, axes = plt.subplots(ncols=1, nrows=1, figsize=(11, 10))
 for (i, country) in enumerate(countries):
     sdf = world_df[world_df['countriesAndTerritories'] == country].sort_values('dateRep', ascending=True)
-    days = pd.Series((sdf['dateRep'] - min_date).values/np.timedelta64(1, 'D'))
-    days = pd.Series((sdf['dateRep'] - min_date).values/np.timedelta64(1, 'D'))
-    if((sdf['total_deaths'] > 10).sum() > 0):
-        zeroday = (sdf[sdf['total_deaths'] > 10]['dateRep'].iloc[0]-min_date)/np.timedelta64(1, 'D')
-    else:
-        zeroday = days.max()
     yval = sdf['total_deaths']
-    deltadays = days-zeroday
-    if(country in hlcountries):
-        color = colors[i]
-        abr = countries2abr.loc[country, 'countryterritoryCode']
-        ind = np.where(hlcountries == country)[0][0]
-        axes.text(1.1*maxx, legendys[ind+1], str(topn-ind)+'. '+abr,
-                  horizontalalignment='left', verticalalignment='center', fontsize=14)
-        plt.arrow(deltadays.max(), yval.iloc[-1],
-                  1.04*maxx-deltadays.max(), legendys[ind+1]-yval.iloc[-1],
-                  clip_on=False, shape='right', ls=':', color=color, alpha=0.75)
-        geoid = countries2id.loc[country, 'geoId'].lower()
-        if(geoid == 'uk'):
-            geoid = 'gb'
-        flagpath = 'circle-flags/flags/'+ geoid +'.png'
-        if(not os.path.isfile(flagpath)):
-            print(flagpath)
-        flagimg = mpimg.imread(flagpath)
-        zoom = 0.05
-        im = OffsetImage(flagimg, zoom=zoom)
-        ab = AnnotationBbox(im, (1.06*maxx, legendys[ind+1]), xycoords='data',
-                            frameon=False, annotation_clip=False)
-        axes.add_artist(ab)
-    else:
-        color = (0.8, 0.8, 0.8, 0.5)
-    sizes = 2
+    (days, deltadays) = get_days(sdf)
+    color = country_legend(axes, country, hlcountries, legendys, yval, colors=colors, maxx=maxx)    
     axes.plot(deltadays, yval, 'o-', c=color, markersize=sizes, markeredgecolor=color)
 local_axes_formatting(axes, "Days since reaching 10 deaths", "Total Reported Deaths", xlim1=0, xlim2=maxx, ylim1=miny, ylim2=maxy, logx=False, fs=20)
 axes.set_title('Top 20 countries in total reported deaths', fontsize=24)
@@ -201,38 +151,9 @@ legendys = np.logspace(np.log10(miny), np.log10(maxy), len(hlcountries)+2)
 fig, axes = plt.subplots(ncols=1, nrows=1, figsize=(11, 10))
 for (i, country) in enumerate(countries):
     sdf = world_df[world_df['countriesAndTerritories'] == country].sort_values('dateRep', ascending=True)
-    days = pd.Series((sdf['dateRep'] - min_date).values/np.timedelta64(1, 'D'))
-    days = pd.Series((sdf['dateRep'] - min_date).values/np.timedelta64(1, 'D'))
-    if((sdf['total_deaths'] > 10).sum() > 0):
-        zeroday = (sdf[sdf['total_deaths'] > 10]['dateRep'].iloc[0]-min_date)/np.timedelta64(1, 'D')
-    else:
-        zeroday = days.max()
     yval = sdf['deaths_rolling']
-    deltadays = days-zeroday
-    if(country in hlcountries):
-        color = colors[i]
-        abr = countries2abr.loc[country, 'countryterritoryCode']
-        ind = np.where(hlcountries == country)[0][0]
-        axes.text(1.1*maxx, legendys[ind+1], str(topn-ind)+'. '+abr,
-                  horizontalalignment='left', verticalalignment='center', fontsize=14)
-        plt.arrow(deltadays.max(), yval.iloc[-1],
-                  1.04*maxx-deltadays.max(), legendys[ind+1]-yval.iloc[-1],
-                  clip_on=False, shape='right', ls=':', color=color, alpha=0.75)
-        geoid = countries2id.loc[country, 'geoId'].lower()
-        if(geoid == 'uk'):
-            geoid = 'gb'
-        flagpath = 'circle-flags/flags/'+ geoid +'.png'
-        if(not os.path.isfile(flagpath)):
-            print(flagpath)
-        flagimg = mpimg.imread(flagpath)
-        zoom = 0.05
-        im = OffsetImage(flagimg, zoom=zoom)
-        ab = AnnotationBbox(im, (1.06*maxx, legendys[ind+1]), xycoords='data',
-                            frameon=False, annotation_clip=False)
-        axes.add_artist(ab)
-    else:
-        color = (0.8, 0.8, 0.8, 0.5)
-    sizes = 2
+    (days, deltadays) = get_days(sdf)
+    color = country_legend(axes, country, hlcountries, legendys, yval, colors=colors, maxx=maxx)
     axes.plot(deltadays, yval, 'o-', c=color, markersize=sizes, markeredgecolor=color)
 local_axes_formatting(axes, "Days since reaching 10 deaths", "Deaths per day (7 days rolling average)", xlim1=0, xlim2=maxx, ylim1=miny, ylim2=maxy, logx=False, fs=20)
 axes.set_title('Top 20 countries in reported deaths per day', fontsize=24)
